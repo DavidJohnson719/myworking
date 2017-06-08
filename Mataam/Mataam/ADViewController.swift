af79f2c8e598956d253a32e6cfb4c806498b4837
AndreszGolanski <andresz.golanski@yandex.com>
2017-06-08 21:06:34 +0200

    First
    From Splash to AD

---
+++ Mataam/Mataam/ADViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mataam
+//
+//  Created by AndreszGolanski on 6/7/17.
+//  Copyright © 2017 AndreszGolanski. All rights reserved.
+//
+
+import UIKit
+
+class ADViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
